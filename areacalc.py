687da0e2d7022fbac4728c725c2383ce0029f73f
Leo Ashcraft <leoashcraft@gmail.com>
2021-07-08 05:42:28 -0500

    Submitting my Python homework for Week2 Day2

---
+++ areacalc.py
@@ -0,0 +1,12 @@
+# from math import pi
+from math import floor, pi
+
+def sqft(l,w):
+        a = 0
+        a = a + (l * w)
+        print("%.2f" % a)
+        
+def circ(r):
+        a = 0
+        a = a + (2 * pi * r)
+        print("%.2f" % a)
